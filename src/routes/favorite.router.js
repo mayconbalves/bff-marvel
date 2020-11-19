5dc213a34dfd698726acc71e98591ebfac5ce7d3
mayconbalves <mayconmustaine@hotmail.com>
2020-11-19 11:44:05 -0300

    feat(favorite): create get, post and update routes

---
+++ src/routes/favorite.router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const favorite_controller = require('../controllers/favorite.controller')
+
+router.get('/list', favorite_controller.list)
+router.post('/create', favorite_controller.create)
+router.put('/:id/update', favorite_controller.update)
+
+module.exports = router
